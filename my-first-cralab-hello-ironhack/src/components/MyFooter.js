63b42b108440880db9ec4cdb50eb5a56f7cb057a
rodpru <rgp.prt@gmail.com>
2020-11-23 17:59:37 +0000

    footer centered

--- my-first-cralab-hello-ironhack/src/components/MyFooter.js
+++ my-first-cralab-hello-ironhack/src/components/MyFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function myFooter() {
     return (
-        <div>
+        <div className="footer-imgs">
             <img src="../../icon1.png" />
             <img src="../../icon2.png" />
             <img src="../../icon3.png" />
